2c0a6af794f50f60ccd95397a335139f2dad844e
c-bailey1 <christopher.bailey@quinnipiac.edu>
2026-05-03 16:44:32 -0400

    Add files via upload

---
+++ pools/517/517.js
@@ -0,0 +1,17 @@
+window.CPPool517Init = function() { cp.model['517Data']={
+};cp.poolSlideResources['pool517']={
+};
+cp.model.data['517']={
+pqs:[],
+gqs:[],
+sqs:[],
+rgqs:[],
+rsqs:[],
+hasCC:false
+};
+cp.poolResources["517Images"]=[];
+cp.poolResources["517Videos"]=[
+];
+cp.poolResources["517SlideVideos"]=[
+];
+}
